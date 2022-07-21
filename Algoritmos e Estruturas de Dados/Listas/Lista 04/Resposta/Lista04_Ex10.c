3f466e40c0f831a124836a6412a5eba5ecb3f2f9
HackbartPR <cgharckbart@gmail.com>
2022-07-21 08:54:51 -0300

    Finalizacao da Lista 04

--- Algoritmos e Estruturas de Dados/Listas/Lista 04/Resposta/Lista04_Ex10.c
+++ Algoritmos e Estruturas de Dados/Listas/Lista 04/Resposta/Lista04_Ex10.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 
-#define _MAX 5
+#define _MAX 10
 
 int main(){
     int array[_MAX];
-    int small = 0;
-    int index = 0;
+    int isContinue = 0;
+    int count = 0;
 
     for(int i=0; i<_MAX; i++){
         printf("DIGITE O %d%c DO VETOR: ", i+1, 167);
         scanf("%d", &array[i]);
-
-        if(i == 0) small = array[i];
     }
 
+    while(isContinue < (_MAX - 1)){
+        int temp = 0;
 
+        if(count == _MAX -1) count = 0;
+
+        if(array[count] > array[count+1]){
+            temp = array[count+1];
+            array[count+1] = array[count];
+            array[count] = temp;
+            isContinue = 0;
+        }
+        else isContinue++;
+
+        count++;
+    }
+
+    for(int i=0; i<_MAX; i++)
+        printf("VETOR ORDENADO: %d\n", array[i]);
 
     return 0;
 }
